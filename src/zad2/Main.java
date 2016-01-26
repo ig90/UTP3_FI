cfb2d4e9bb88fa4e2a9471963114d5da0aed0d90
Igor <igor.filipiuk@gmail.com>
2016-01-26 13:20:31 +0100

    Create Main.java

---
+++ src/zad2/Main.java
@@ -0,0 +1,20 @@
+/**
+ *
+ *  @author Filipiuk Igor S7334
+ *
+ */
+
+package zad2;
+
+
+public class Main { 
+	
+	    public static void main(String[] args){
+	      Serwer serv = new Serwer(2222);
+	      serv.runServer();
+	      
+	 
+	      
+      
+	    }
+	  }
