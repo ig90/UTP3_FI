35cf41ff8ad0d3eff86088c8637c21b8ef37bba8
Igor <igor.filipiuk@gmail.com>
2016-01-26 13:24:15 +0100

    Create Main.java

---
+++ src/zad4/Main.java
@@ -0,0 +1,18 @@
+/**
+ *
+ *  @author Filipiuk Igor S7334
+ *
+ */
+
+package zad4;
+
+
+public class Main {
+  public static void main(String[] args) {
+    Author autor = new Author(args);
+    new Thread(autor).start();
+    new Thread(new Writer(autor)).start();
+  }
+}
+
+
