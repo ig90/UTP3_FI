7b7b2befdfccc07e2c2ef6376f4a898d4a20eab2
Igor <igor.filipiuk@gmail.com>
2016-01-26 13:24:37 +0100

    Create Author.java

---
+++ src/zad4/Author.java
@@ -0,0 +1,33 @@
+/**
+ *
+ *  @author Filipiuk Igor S7334
+ *
+ */
+
+package zad4;
+
+import java.util.concurrent.BlockingQueue;
+
+
+public class Author implements Runnable {
+	
+	 private BlockingQueue bq = null;
+	
+	public Author(String[] args){
+		
+
+	}
+	@Override
+	public void run() {
+		
+		
+		
+	}
+	
+	 public void setBlockingQueue(BlockingQueue bq) {
+		 this.bq = bq;
+
+	 }
+	
+}  
+
